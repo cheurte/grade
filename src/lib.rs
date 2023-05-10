3b41304ce388c46c008971b6dc11405387786fb3
cheurte <corentin.heurte@gmail.com>
2023-05-10 16:39:37 +0200

    adding start of tab creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,154 @@
-use std::path::Path;
-use latex::{DocumentClass, Element, Document, Section, Align, print};
+use latex::{print, Document, DocumentClass, Element, Section};
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufReader, Write};
+use std::path::Path;
 
-use std::collections::HashMap;
-use std::fs::write;
-use latexcompile::{LatexCompiler, LatexInput};
+// use latexcompile::{LatexCompiler, LatexInput};
+// use std::collections::HashMap;
+// use std::fs::write;
 
-use calamine::{Reader, open_workbook, Xlsx, DataType};
+use calamine::{open_workbook, DataType, Reader, Xlsx};
 
-use serde::{Deserialize, de::value};
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ConfigXlsx {
-    pub pdf_file: Vec<PdfFile>, 
+    pub pdf_file: Vec<PdfFile>,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PdfFile {
-    pdf_name: String,
+    _pdf_name: String,
     sources: Vec<String>,
     sheets_name: Vec<String>,
-    products: Vec<String>,
+    pub products: Vec<String>,
     categories: Vec<String>,
 }
 
-fn create_tabular(columns: String, values: Vec<String>)->String{
-    let values = values.join(" & ");
-    
-    let tabular = &format!(
-        "\\begin{{tabular}}{{{}}}\n\
-       {} \n\
-        \\end{{tabular}}"
-        , columns, values);
-    tabular.to_string()
+#[derive(Debug)]
+pub enum AlignTab {
+    C,
+    R,
+    L,
 }
 
-/// Create the string that will be compiled. 
+#[derive(Debug)]
+pub struct TabularTex {
+    align: AlignTab,
+    nb_col: usize,
+}
+
+impl TabularTex {
+    pub fn new() -> Self {
+        Self {
+            align: AlignTab::C,
+            nb_col: 0,
+        }
+    }
+    pub fn from(align: AlignTab, nb_col: usize) -> Self {
+        Self { align, nb_col }
+    }
+
+    pub fn define_column(self) -> String {
+        let mut column_definition: String = String::new();
+        let align = match self.align {
+            AlignTab::C => String::from("c"),
+            AlignTab::L => String::from("l"),
+            AlignTab::R => String::from("r"),
+        };
+        for _ in 0..self.nb_col {
+            column_definition.push_str(&format!("X {} ", align)[..]);
+        }
+        println!("{column_definition:?}");
+        column_definition
+    }
+
+    // fn create_tabularx(nb_col: usize) -> String {
+    //     // let tabular = String::from("\\begin{{tabularx}}{{\textwidth}}{}")
+    //     // let values = values.join(" & ");
+    //     // let
+    //     // let tabular = &format!(
+    //     //     "\\begin{{tabular}}{{{}}}\n\
+    //     //    {} \n\
+    //     //     \\end{{tabular}}",
+    //     //     columns, values
+    //     // );
+    //     // let tabular = String::from("");
+    //     // tabular.to_string()
+    //     String::new()
+    // }
+}
+
+// pub fn begin_section_wo_param(name: String, content: String) -> String {
+//     let section = &format!(
+//         "\\begin{{{}}}\n\
+//             {}\
+//         \\end{{{}}}",
+//         name, content, name
+//     );
+//     section.to_string()
+// }
+
+/// Create the string that will be compiled.
 /// This function will be depending on json files later. -> Todo
 ///
-pub fn page_blue_print()->String{
+pub fn page_blue_print() -> String {
     let mut doc = Document::new(DocumentClass::Report);
     doc.preamble.title("Template document");
     doc.push(Element::TitlePage);
+    doc.push(Element::ClearPage);
+    // doc.push(
+    // &TabularTex::begin_section_wo_param("center".to_string(), "ceci est un test".to_string())
+    // [..],
+    // );
 
-    let tab = &create_tabular("c c".to_string(), vec!["a".to_string(), "b".to_string()])[..];
+    println!("{doc:?}");
 
-    doc.push(tab);
     print(&doc).unwrap()
 }
 
-/// Handle the tex creation. 
+/// Handle the tex creation.
 /// Seperate function to handle the windows server lately -> ToDo
-/// 
-pub fn create_tex_file(rendered: String)->std::io::Result<()> {
+///
+pub fn create_tex_file(rendered: String) -> std::io::Result<()> {
     let mut f = File::create("output/report.tex")?;
     write!(f, "{}", rendered)?;
     Ok(())
 }
 
-/// Create the pdf file by creating a clean environment and executing the 
+/// Create the pdf file by creating a clean environment and executing the
 /// compiler in the assets directory. Tested only on linux distribution yet
-/// with latexmk compiler. 
+/// with latexmk compiler.
 ///
 /// Again, to be tested on a windows server -> Todo
-/// 
-pub fn create_pdf()->std::io::Result<()>{
-    let mut dict:HashMap<String,String> = HashMap::new();
-    dict.insert("test".into(), "Minimal".into());
-    // provide the folder where the file for latex compiler are found
-    let input = LatexInput::from("assets");
-    // create a new clean compiler enviroment and the compiler wrapper
-    let compiler = LatexCompiler::new(dict).unwrap();
-    // run the underlying pdflatex or whatever
-    let result = compiler.run("/home/cheurte/Documents/grade/output/report.tex", &input).unwrap();
+///
+pub fn create_pdf() -> std::io::Result<()> {
+    // let mut dict = HashMap::new();
+    // dict.insert("test".into(), "Minimal".into());
+    // // provide the folder where the file for latex compiler are found
+    // let input = LatexInput::from("assets");
+    // // create a new clean compiler enviroment and the compiler wrapper
+    // let compiler = LatexCompiler::new(dict).unwrap();
+    // // run the underlying pdflatex or whatever
+    // let result = compiler.run("assets/test.tex", &input).unwrap();
+
     // // copy the file into the working directory
-    let output = ::std::env::current_dir().unwrap().join("output/test.pdf");
-    assert!(write(output, result).is_ok());
+    // let output = ::std::env::current_dir()
+    //     .expect("problem with current dir ??")
+    //     .join("out.pdf");
+    //
+    // assert!(write(output, result).is_ok());
     Ok(())
 }
 
-impl ConfigXlsx{
-    pub fn new()->Self {
-       Self { pdf_file: vec![] }
+impl ConfigXlsx {
+    pub fn new() -> Self {
+        Self { pdf_file: vec![] }
     }
 
-    pub fn from(path: &str)->Self{
+    pub fn from(path: &str) -> Self {
         let path = Path::new(path);
         let file = File::open(path).expect("Problem with the file");
         let config: ConfigXlsx = serde_json::from_reader(file).expect("error in the reading");
@@ -96,99 +157,143 @@ impl ConfigXlsx{
 }
 
 impl PdfFile {
+    pub fn get_workbook(&self, id_woorkbook: usize) -> Xlsx<BufReader<File>> {
+        let path = self.sources.iter().nth(id_woorkbook).unwrap();
+        let workbook: Xlsx<_> = open_workbook(path)
+            .expect("Cannot open workbook. It is probably an error in the path of the workbook.");
+        workbook
+    }
+
+    pub fn get_worksheet(&self, id_worksheet: usize) -> &String {
+        self.sheets_name.iter().nth(id_worksheet).expect(
+            "Please make sur the name between the worksheet and
+                the name given in the configuration file are the same.",
+        )
+    }
     /// Function to search the key elements of the file.
-    /// Suppose to depend on a json file to know wich line or row can be red.   
-    pub fn get_coordinates(&self)->(Vec<(usize, usize)>, Vec<(usize, usize)>){
+    /// Suppose to depend on a json file to know wich line or row can be reed.
+    pub fn get_prod_categ_coordinates(&self) -> (Vec<(usize, usize)>, Vec<(usize, usize)>) {
         // To be changed to handle errors
-        let path = self.sources.iter().nth(0).unwrap();
-        let sheet = self.sheets_name.iter().nth(0).unwrap();
-        let products = &self.products;
-        let categories = &self.categories;
-        let mut workbook: Xlsx<_> = open_workbook(path)
-                                        .expect("Problem by reading file");
-        let mut categories_coord: Vec<(usize, usize)>=vec![];
-        let mut products_coord: Vec<(usize, usize)>=vec![];
-        match workbook.worksheet_range(&sheet) {
+        let mut workbook = self.get_workbook(0);
+
+        let mut categories_coord: Vec<(usize, usize)> = vec![];
+        let mut products_coord: Vec<(usize, usize)> = vec![];
+
+        match workbook.worksheet_range(self.get_worksheet(0)) {
             Some(Ok(range)) => {
-                for row in 0..range.get_size().0{
-                    for col in 0..range.get_size().1{
+                for row in 0..range.get_size().0 {
+                    for col in 0..range.get_size().1 {
                         let value = range.get_value((row as u32, col as u32));
-                        if value != Some(&DataType::Empty){
-                            for category in categories{
-                                if &value.unwrap().to_string() == category{
+                        if value != Some(&DataType::Empty) {
+                            for category in &self.categories {
+                                if &value.unwrap().to_string() == category {
                                     categories_coord.push((row, col));
                                 }
                             }
-                            for product in products{
-                                if &value.unwrap().to_string() == product{
+                            for product in &self.products {
+                                if &value.unwrap().to_string() == product {
                                     products_coord.push((row, col));
                                 }
                             }
                         }
                     }
                 }
-            },
+            }
             Some(Err(e)) => println!("{e:?}"),
-            None => println!("Sheets name unknown. Maybe check the name in the config file")
+            None => println!("Sheets name unknown. Maybe check the name in the config file"),
         }
         (categories_coord, products_coord)
     }
     /// Search the range of categories, to know where to stop
     /// Take the beginning corrdinates of categories, and return the end coordinates
-    pub fn get_parameters_range(&self, categories_coord: &Vec<(usize, usize)>) 
-        -> Vec<(usize, usize)> 
-    {
-        let sheet = self.sheets_name.iter().nth(0).unwrap();
-        let path = self.sources.iter().nth(0).unwrap();
-
+    pub fn get_parameters_range(
+        &self,
+        categories_coord: &Vec<(usize, usize)>,
+    ) -> Vec<(usize, usize)> {
+        // let sheet = self.sheets_name.iter().nth(0).unwrap();
         let mut end_categories: Vec<(usize, usize)> = vec![];
-        let mut workbook: Xlsx<_> = open_workbook(path)
-                                        .expect("Problem by reading file");
-        match workbook.worksheet_range(&sheet) {
+        let mut workbook = self.get_workbook(0);
+
+        match workbook.worksheet_range(&self.get_worksheet(0)) {
             Some(Ok(range)) => {
-                for (category_row, category_col) in categories_coord.into_iter(){
-                    let mut col = category_col+1;
+                for (category_row, category_col) in categories_coord.into_iter() {
+                    let mut col = category_col + 1;
                     loop {
-                        if range.get_value((*category_row as u32, col as u32)) != Some(&DataType::Empty) || col > range.get_size().1{
-                            break
+                        if range.get_value((*category_row as u32, col as u32))
+                            != Some(&DataType::Empty)
+                            || col > range.get_size().1
+                        {
+                            break;
                         }
-                        col +=1;
+                        col += 1;
                     }
-                    end_categories.push((*category_row, col-1));
-                }                
-            },
+                    end_categories.push((*category_row, col - 1));
+                }
+            }
             Some(Err(e)) => println!("{e:?}"),
-            None => println!("Sheets name unknown. Maybe check the name in the config file")
+            None => println!("Sheets name unknown. Maybe check the name in the config file"),
         }
         end_categories
     }
     /// Get parameters names
     /// Take the indices of values to get them.
-    pub fn get_parameters_name(&self, start_categ_coord: &Vec<(usize, usize)>, end_categ_coord: &Vec<(usize, usize)>)->Vec<Vec<String>>{
-        assert_eq!(start_categ_coord.len(), end_categ_coord.len()); 
-        let sheet = self.sheets_name.iter().nth(0).unwrap();
-        let path = self.sources.iter().nth(0).unwrap();
-        let mut workbook: Xlsx<_> = open_workbook(path)
-                                        .expect("Problem by reading file");
+    pub fn get_parameters_name(
+        &self,
+        start_categ_coord: &Vec<(usize, usize)>,
+        end_categ_coord: &Vec<(usize, usize)>,
+    ) -> Vec<Vec<String>> {
+        assert_eq!(start_categ_coord.len(), end_categ_coord.len());
+
+        let mut workbook = self.get_workbook(0);
         let mut output: Vec<Vec<String>> = vec![];
-        match workbook.worksheet_range(&sheet) {
+
+        match workbook.worksheet_range(self.get_worksheet(0)) {
             Some(Ok(range)) => {
                 let it = start_categ_coord.iter().zip(end_categ_coord.iter());
                 for (_, (start_coord, end_coord)) in it.enumerate() {
                     let mut parameters: Vec<String> = vec![];
-                    for col in start_coord.1+2..end_coord.1{
-                        parameters
-                            .push(range
-                                .get_value(((start_coord.0+1) as u32, col as u32))
-                                    .unwrap()
-                                    .to_string())
+                    for col in start_coord.1..end_coord.1 + 1 {
+                        parameters.push(
+                            range
+                                .get_value(((start_coord.0 + 1) as u32, col as u32))
+                                .unwrap()
+                                .to_string(),
+                        )
                     }
                     output.push(parameters);
                 }
-            },
+            }
             Some(Err(e)) => println!("{e:?}"),
-            None => println!("Sheets name unknown. Maybe check the name in the config file")
+            None => println!("Sheets name unknown. Maybe check the name in the config file"),
         }
         output
+    }
+
+    pub fn get_values_from_parameters(
+        &self,
+        product_coordinates: (usize, usize),
+        start_categ_coord: &Vec<(usize, usize)>,
+        end_categ_coord: &Vec<(usize, usize)>,
+    ) -> Vec<Vec<String>> {
+        let mut workbook = self.get_workbook(0);
+        let mut out: Vec<Vec<String>> = Vec::new();
+        match workbook.worksheet_range(self.get_worksheet(0)) {
+            Some(Ok(range)) => {
+                for param in 0..start_categ_coord.len() {
+                    let mut parameters: Vec<String> = vec![];
+                    for y in start_categ_coord.iter().nth(param).unwrap().1
+                        ..end_categ_coord.iter().nth(param).unwrap().1 + 1
+                    {
+                        let x = product_coordinates.0;
+                        parameters.push(range.get_value((x as u32, y as u32)).unwrap().to_string());
+                    }
+                    out.push(parameters);
+                }
+            }
+            Some(Err(e)) => println!("{e:?}"),
+            None => println!("Sheets name unknown. Maybe check the name in the config file"),
+        }
+        // return vec![vec![]];
+        out
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
-// use grade::{page_blue_print, create_pdf, create_tex_file};
-use grade::ConfigXlsx;
+// use grade::{create_pdf, create_tex_file, page_blue_print};
+use grade::{AlignTab, TabularTex};
+// use grade::ConfigXlsx;
+
+// use std::process::Command;
 
 fn main() {
+    let tab: TabularTex = TabularTex::from(AlignTab::C, 3);
+    tab.define_column();
     // let content = page_blue_print();
     // println!("{content:?}");
-    // match create_tex_file(content){
+    // match create_tex_file(content) {
     //     Ok(_) => println!("ok"),
     //     Err(e) => println!("{e:?}"),
     // }
+    // let exit_status = Command::new("latexmk")
+    //     .arg("output/report.tex")
+    //     .arg("--output-directory=output/")
+    //     .status()
+    //     .unwrap();
+    // assert!(exit_status.success());
+
     // create_pdf().expect("error creating pdf");
-    let configs = ConfigXlsx::from("config/config_source.json");
-    for pdf_file in configs.pdf_file.iter(){
-        let (cat_coord, _): (Vec<(usize, usize)>, Vec<(usize, usize)>) = pdf_file.get_coordinates();
-        // println!("{cat_coord:?}");
-        let end_categories = pdf_file.get_parameters_range(&cat_coord);
-        // println!("{:?}", end_catego as u32ries);
-        let param = pdf_file.get_parameters_name(&cat_coord, &end_categories);
-        println!("{param:?}");
-        
-        break;
-    }
+    // let configs = ConfigXlsx::from("config/config_source.json");
+    // for pdf_file in configs.pdf_file.iter() {
+    //     let (begin_categories, prod_coords): (Vec<(usize, usize)>, Vec<(usize, usize)>) =
+    //         pdf_file.get_prod_categ_coordinates();
+    //     // println!("{begin_categories:?}, {prod_coords:?}");
+    //     let end_categories = pdf_file.get_parameters_range(&begin_categories);
+    //     // println!("{:?}", end_categories);
+    //     let _param = pdf_file.get_parameters_name(&begin_categories, &end_categories);
+    //     // println!("{param:?}");
+    //
+    //     for (i, prod_coord) in prod_coords.iter().enumerate() {
+    //         let data = pdf_file.get_values_from_parameters(
+    //             *prod_coord,
+    //             &begin_categories,
+    //             &end_categories,
+    //         );
+    //         println!("{data:?}");
+    //     }
+    //     println!("{_param:?}");
+    //     break;
+    // }
 }
